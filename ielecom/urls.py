b45b470fb3fbf68453bde2bcdf7630ee91dce954
Ehsan <ehsan@ielecom.org>
2014-10-11 16:53:03 +0330

    rm hitcount

--- ielecom/urls.py
+++ ielecom/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 from ielecom.view import *
 from maintenance.views import *
 from django.contrib import admin
-from hitcount.views import update_hit_count_ajax
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -12,7 +11,6 @@ urlpatterns = patterns('',
     url(r'^contact$', contact_tp),
     url(r'^contact_form', contact),
     url(r'^about', about),
-    url(r'^ajax/hit/$', update_hit_count_ajax, name='hitcount_update_ajax'),
     url(r'^download/$',download),
 
 
